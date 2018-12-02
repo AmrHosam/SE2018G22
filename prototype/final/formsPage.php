2e028fb0abf8bbcce438d9f5bfbe81ef2516c836
AmrHosam <amrhossam97@gmail.com>
2018-12-02 22:57:04 +0200

    add final prototype

---
+++ prototype/final/formsPage.php
@@ -0,0 +1,186 @@
+<?php   include_once('navSign.php');?>
+<!doctype html>
+<html dir="auto" lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
+<link href="css/bootstrap.css" rel="stylesheet" />
+<link href="css/bootstrap-rtl.css" rel="stylesheet" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <title>Hello, world!</title>
+  </head>
+  <style type="text/css">
+  #requests{font-size:180%;}
+  h1{font-size:120%;
+  display:block;
+  margin:auto;}
+:lang(ar) 	{font-family: "Scheherazade",serif; 
+                 font-size: 105%;}
+		  .jumbotron{background-color:white;}
+		  .container{background-color:#f0f2f3;
+		  border-radius:20px;}
+		  .edge{margin-right:2px;
+		  margin-bottom:0;}
+		  .btn{text-decoration:none !important;}
+		  .container{padding:20px;}
+  </style>
+  <body>
+  
+
+<div class="bimg">
+</div>
+
+<div dir="rtl" lang="ar" class="jumbotron">
+  
+
+<div dir="rtl" class="container"> 
+ <div class="form-group row" dir="rtl"  id="requests" >
+    <h1 > Requests</h1>
+</div>
+
+  <hr class="my-4">
+ 
+  
+  <form>
+   <div class="form-group row" dir="rtl">
+    <label for="exampleFormControlSelect1" dir="ltr" style="margin-right:10px">choose your request | اختر طلبك</label>
+   
+	<select lang="ar" class="form-control form-control-lg" id="exampleFormControlSelect1" rows="3">
+     <option value="1">شهادة تخرج</option>
+  <option value="2">كارنيه</option>
+  <option value="3">كارنيه مترو</option>
+ <option value="5">شهادة قيد</option>
+   <option value="4">اخر</option>
+    </select>
+	
+  </div>
+</select>
+  </form>
+  <div class="form-group row edge" dir="rtl" >
+  <p class="lead">
+   <a class="btn btn-primary btn-lg" href="#" role="button" onclick="showDiv()" >تنفيذ</a>
+  </p>
+  </div>
+
+
+
+
+
+
+ 
+
+
+  
+  
+ 
+ 
+ <div id="welcomeDiv"  style="display:none;" class="answer_list" >
+<form> 
+ <div class="form-row" >
+    <div class="form-group col-md-6" >
+	<div class="form-group row edge" dir="rtl" >
+      <label for="inputEmail4" >الاسم باللغة العربية</label> 
+	  </div>
+	  
+      <input type="email" class="form-control" id="inputEmail4" >
+ </div>
+    <div class="form-group col-md-6">
+	<div class="form-group row edge" dir="rtl">
+      <label for="inputEmail4">الاسم باللغة الانجليزية</label>
+      </div>
+	  <input type="email" class="form-control" id="inputEmail4"  dir="ltr">
+    </div>
+  </div>
+ 
+    
+ <div class="form-group" dir="rtl">
+    <div class="form-group row edge" dir="rtl">
+	<label for="inputAddress" >العنوان</label>
+	</div>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  
+ </div>
+ 
+<div class="form-group">
+<div class="form-group row edge" dir="rtl">
+    <label for="exampleFormControlTextarea1">سبب الاستخراج</label>
+	</div>
+	<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  <div class="row">
+    <div class="form-group col-md-3">
+	<div class="form-group row edge" dir="rtl">
+      <label for="inputCity">رقم الهاتف</label>
+</div>    
+	<input type="text" class="form-control" id="inputCity">
+    
+	</div>
+    <div class="form-group col-md-3">
+	<div class="form-group row edge" dir="rtl">
+      <label for="inputState">الفرقة</label>
+	  </div>
+      <select id="inputState" class="form-control" style="font-size:90%">
+        <option selected>الاعدادية</option>
+        <option>الاولى</option>
+		<option>الثانية</option><option>الثالثة</option><option>الرابعة</option>
+      </select>
+    </div>
+	
+	<div class="form-group col-md-3">
+	<div class="form-group row edge" dir="rtl">
+      <label for="inputZip" >القسم</label>
+	  </div>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+	
+	
+    <div class="form-group col-md-3">
+	<div class="form-group row edge" dir="rtl">
+      <label for="inputZip" >تاريخ الميلاد</label>
+	  </div>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+   <div class="form-group carneh" dir="rtl" >
+  <div class="form-group row edge" dir="rtl">
+      <label for="inputZip" >شهادة الميلاد</label>
+	  </div>
+  <div class="custom-file">
+  <input type="file" class="custom-file-input" id="customFile">
+  <label class="custom-file-label" for="customFile">Choose file</label>
+</div>
+  </div>
+  
+ <div class="form-group row edge" dir="rtl">
+  <button type="submit" class="btn btn-primary">تأكيد</button>
+  </div>
+  
+</form>
+<script type="text/javascript">
+function showDiv() {
+   document.getElementById('welcomeDiv').style.display = "block";
+}
+$(function() { 
+   
+    $('#exampleFormControlSelect1').change(function(){
+        if($('#exampleFormControlSelect1').val() == '1') {
+
+            $('.carneh').show();
+        } else {
+            $('.carneh').hide(); 
+        } 
+    });
+});	
+
+</script>
+</div>
+ 
+</div>
+</div>  
+  </body>
+</html>
---
+++ prototype/final/navSign.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Affairs</title>
+           <link rel="shortcut icon" href="icon.png">
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+</head>
+     <style type="text/css">
+         .smallImge{
+             height: 49px;
+             width: 49px;
+              position: relative;
+         }
+          .Icons_Button{
+              height: 150px;
+              width: 150px;
+              position: relative;
+              float: left;
+          }  
+          .events{
+               height: 300px;
+              width: 300px;
+              position: relative;
+              float: left;
+              background-color:aquamarine; 
+              margin: 100px;
+          }
+          #events{
+              background-color: beige;
+              border: solid;
+          }
+          .buttons{
+             border:solid;
+            background-color: #800000;
+            width: 200px;
+             height: 200px;
+              border-radius: 50%;
+               opacity: 1;
+              
+              
+          }
+          .font_button{
+            color:#FFFAFA; 
+            position: relative;
+              float: inherit;
+               opacity: 1;
+          }
+          #rowstyle{
+              position: relative;
+             margin-top: 15%;
+              margin-left: 15%; 
+              margin-right: 0;
+              
+          }
+          .Request_order{
+              margin-left: 25%;
+              margin-top: 10%;
+              opacity: 1;
+              position: relative;
+              float: left;
+          }
+           .question{
+              margin-left: 50%;
+              margin-top: 0%;
+                opacity: 1 !important ;
+                 position: relative;
+              float: left;
+          }
+           .profile{
+              margin-left: 75%;
+              margin-top: 0px;
+                opacity: 1;
+                 position: relative;
+              float: left;
+          }
+          .navbar{
+              background-color: #800000 !important;
+              height: 50px;
+          }
+          .nav-link{
+              font-weight: 700;
+              font-size: 18px;
+              color: #FFFAFA !important;
+              margin-left: 50px;
+              
+          }
+          .nav-item:hover{
+              cursor: pointer;
+              color: black !important;
+          }
+          .fa:hover{
+              color: black !important; 
+          }
+          #sign{
+            margin-bottom: 30px;  
+          }
+}
+          .bimg{
+              opacity: 1;
+          }
+         .buttons:hover
+          {
+            border-style: inset !important;
+              cursor: pointer;
+          }
+          .nav-link:hover{
+              color: black !important;
+          }
+                    /*comment*/
+          @media only screen and (max-width: 991px){
+.dropdown-menu
+{
+  padding-top: 0px !important;
+  margin-top: 0px !important;
+}
+.passwordinput
+{
+  margin-top: 5% !important;
+}
+.signbutton
+{
+  margin-top: 2% !important;
+  margin-bottom: 2% !important;
+
+}
+.signupbutton
+{
+  margin-top: 1% !important;
+    margin-bottom: 1% !important;
+
+}
+}
+@media only screen and (max-width: 765px){
+.dropdown-menu
+{
+  padding-top: 0px !important;
+  margin-top: 0px !important;
+}
+.passwordinput
+{
+  margin-top: 2% !important;
+  margin-left: 142px !important;
+}
+.signbutton
+{
+  margin-top: 2% !important;
+  margin-bottom: 2% !important;
+
+}
+.signupbutton
+{
+  margin-top: 2% !important;
+}
+}
+@media only screen and (max-width: 575px){
+.dropdown-menu
+{
+/*  padding-top: 0px !important;
+*/}
+.emailinput
+{
+  margin-top: 0% !important;
+}
+.passwordinput
+{
+  margin-top: 1.5% !important;
+  margin-left: 0% !important;
+}
+.signbutton
+{
+  margin-top: 2% !important;
+  margin-bottom: 2% !important;
+
+}
+.signupbutton
+{
+  margin-top: 1% !important;
+    margin-bottom: 1% !important;
+
+}
+}
+     </style>
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light">
+  <a><img src="studentAfaairs_icon.png" class="smallImge"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="homesign.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+     
+      
+         <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">About</a>
+      </li>
+    </ul>       
+          <li style="list-style-type: none;" class="nav-item dropdown" style="" >
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fa fa-bell-o" style="font-size:30px;color:white"></i>
+            </a>
+            <div style="" class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li style="list-style-type: none; margin-right: 30px;" class="nav-item dropdown" style="" >
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Welcome
+        </a>
+         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="profile.php">Profile</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Logout</a>
+        </div>
+      </li>
+  </div>
+</nav>
+</body>
+</html>
