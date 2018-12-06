48fa01226d28785329631311f436f37d5472ffd7
Amr Hossam <amrhossam97@gmail.com>
2018-12-06 21:04:33 +0200

    Add requests page (employee side)

---
+++ prototype/final/requests.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="keyword" content="questions,enquiries,inqiries,studentaffairs">
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Requests</title>
+	<style type="text/css"></style>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+						<!-- bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <!--logo of the website-->
+    <link rel="shortcut icon" href="question.jpg" type="image/x-icon">
+</head>
+<body style="background-color: #D3D3D3">
+<div class="form" style="background-color: white; border-style: outset;">
+	<div class="title">
+		<p style="text-align: center; font-size: 20px; font-weight: 650; margin: 5px;">Certificate of registration</p>
+	</div>
+	<hr style="border-color: black !important; border-width: 1.5px; margin: 5px;">
+	<div class="field" style="margin-top: 10px;">
+		<p style="float: left;margin-right: 10px; font-weight: 550;">Full Name:</p>
+		<p>Amr Hossam Ali</p>
+	</div>
+	<div class="field">
+		<p style="float: left;margin-right: 10px; font-weight: 550;">Year:</p>
+		<p>3</p>
+	</div>
+	<div class="field">
+		<p style="float: left;margin-right: 10px; font-weight: 550;">Department:</p>
+		<p>Electrical</p>
+	</div>
+	<div class="field">
+		<p style="float: left;margin-right: 10px; font-weight: 550;">Major:</p>
+		<p>Computer</p>
+	</div>
+	<div class="field">
+		<p style="float: left;margin-right: 10px; font-weight: 550;">Student ID:</p>
+		<p>15202</p>
+	</div>
+	<div class="field">
+	<img src="question.jpg" style="width:400px;height: 200px; display: block; margin:auto;">
+	</div>
+</div>
+	<input type="submit" name="next" value="Next" style="background-color: transparent; border-radius: 7px; border-color: black;display: block;margin: auto; color: brown; font-weight: 500; font-size: 18px; padding-bottom: 10px; padding-top: 10px; padding-right: 15px; padding-left: 15px; ">
+</div>
+</body>
+</html>
