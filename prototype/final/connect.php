eede2d8a64b3a04627a36c77555278466ab95a2d
Omar Shafik <o.shafik13@gmail.com>
2018-12-11 14:56:45 +0200

    upstream fetch

--- prototype/final/connect.php
+++ prototype/final/connect.php
@@ -3,6 +3,7 @@ $servername = "localhost";
 $username = "omar";
 $password = "sawebsite";
 $dbname = "sawebsite";
+//port=3306
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
