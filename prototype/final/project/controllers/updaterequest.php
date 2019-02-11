a7f0aea5c8f8247fb766592202563816c8407408
omarmagdy217 <36456356+omarmagdy217@users.noreply.github.com>
2019-02-11 19:37:42 +0200

    Update updaterequest.php

--- prototype/final/project/controllers/updaterequest.php
+++ prototype/final/project/controllers/updaterequest.php
@@ -1,13 +1,19 @@
 <?php
+	session_start();
 	include_once('../connect.php');
+	if($_GET['clear']==0){
 	$query = "UPDATE `requests` SET `state`= 1 WHERE id = '".$_GET['id']."'";
 	mysqli_query($link, $query);
 	/*$query = "SELECT * FROM `requests` WHERE `state` = 0";
 	if ($results = mysqli_query($link,$query)){
 	$count = mysqli_num_rows($results);}
 	if($count > 0){*/
-	header('Location: ../requests.php');
+	header('Location: ../requests.php');}
 	/*}
 	else{
 		header('Location: ../homesign.php');}*/
+	else if($_GET['clear']==1){
+	$query = "UPDATE `requests` SET `seen`= 1 WHERE `state` = 1 AND student_id ='" . $_SESSION['id'] . "'";
+	mysqli_query($link, $query);
+	header('Location: ../homesign.php');}
 ?>
